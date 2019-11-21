7d419e0c48688a6f7e43043aca75e7ce44821bdc
gokatz <gokulkathirvel@live.com>
2019-11-21 11:31:16 +0530

    fix assest loading issue (rootURL config)

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -43,11 +43,11 @@ module.exports = function(environment) {
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
-    // Allow ember-cli-addon-docs to update the rootURL in compiled assets
-    ENV.rootURL = '';
-    // here you can enable a production-specific feature
-  }
+  // if (environment === 'production') {
+  //   // Allow ember-cli-addon-docs to update the rootURL in compiled assets
+  //   ENV.rootURL = '';
+  //   // here you can enable a production-specific feature
+  // }
 
   return ENV;
 };
