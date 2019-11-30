c6b732fe3ce57f826235edb2e1dee8cf782170a5
gokatz <gokulkathirvel@live.com>
2019-12-01 01:26:40 +0530

    remove unwanted configs

--- testem.js
+++ testem.js
@@ -7,15 +7,11 @@ module.exports = {
   launch_in_dev: [
     'Chrome'
   ],
-  // browser_paths: {
-  //   "Chromium": "./node_modules/puppeteer/.local-chromium/linux-706915/chrome"
-  // },
   browser_args: {
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI || process.env.GITPOD_HOST ? '--no-sandbox' : null,
-        process.env.GITPOD_HOST ? '--disable-setuid-sandbox' : null,
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
--- tests/dummy/app/templates/docs/installation.md
+++ tests/dummy/app/templates/docs/installation.md
@@ -7,5 +7,5 @@ ember install ember-adaptive-service
 ## Compatibility
 
 * Ember.js v3.4 or above
-* Ember CLI v3.0 or above
+* Ember CLI v2.13 or above
 * Node.js v8 or above
